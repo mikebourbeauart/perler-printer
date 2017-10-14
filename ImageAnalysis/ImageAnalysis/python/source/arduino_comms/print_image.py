8395e303303f7d1e0abfc5f2d6ec47f4ef08fe89
Mike Bourbeau <borbs727@gmail.com>
2017-10-13 23:24:06 -0400

    mght work?

--- ImageAnalysis/ImageAnalysis/python/source/arduino_comms/print_image.py
+++ ImageAnalysis/ImageAnalysis/python/source/arduino_comms/print_image.py
@@ -45,37 +45,40 @@ try:
 	with open(json_file_path, 'r') as handle:
 		json_data = json.load(handle, object_pairs_hook=OrderedDict) # Opens dict in order
 
+	region_count = 0
+	for key in json_data.iteritems():
+
+		region = list(json_data.keys())[region_count]
+		print 'region {0}'.format(region)
+		pixel_count = 0
+		pixel_data = json_data[region]
+		for pos, color in pixel_data.iteritems():
+		
+			# Send pixel pos data to printer
+			ser.write('1')
+
+			print 'pixel {0}'.format(pos)
+			print 'color {0}'.format(color)
+
+			inChar = ser.read()
+			print 'serial {0}'.format(inChar)
+
+			# Wait for response that bead has been dropped
+			while ser.read() != '0':
+				ser.read()
+		
+			# Go to home location and pick up new bead
+			pixel_count += 1
+		region_count += 1
+
 except:
 	logger.warning('No region data found. Run image_split then region_parse to create region data')
 
 #for key, val in json_data.iteritems():
 #print(key)
 #print(val)
-region_count = 0
-for key in json_data.iteritems():
 
-	region = list(json_data.keys())[region_count]
-	print 'region {0}'.format(region)
-	pixel_count = 0
-	pixel_data = json_data[region]
-	for pos, color in pixel_data.iteritems():
-		
-		# Send pixel pos data to printer
-		ser.write('1')
 
-		print 'pixel {0}'.format(pos)
-		print 'color {0}'.format(color)
-
-		inChar = ser.read()
-		print 'serial {0}'.format(inChar)
-
-		# Wait for response that bead has been dropped
-		while ser.read() != '0':
-			ser.read()
-		
-		# Go to home location and pick up new bead
-		pixel_count += 1
-	region_count += 1
 
 
 
--- ImageAnalysis/ImageAnalysis/python/source/image_parsing/region_parse.py
+++ ImageAnalysis/ImageAnalysis/python/source/image_parsing/region_parse.py
@@ -26,55 +26,58 @@ json_file = os.path.join(root_dir, 'resources', 'json_data', 'region_data.json')
 
 json_data = OrderedDict()
 
-for im_file in os.listdir(regions_dir):
-	#im_file = 'test_0-3.bmp'
-	# Open image
-	#im_file = "C:/Users/borbs/OneDrive/04_Job Stuff/03_Work Stuff/PERLER_BEAD_MACHINE_00000/regions/test_0-0.bmp"
-	im = Image.open(os.path.join(regions_dir, im_file))
-	px = im.load()
+try:
+	for im_file in os.listdir(regions_dir):
+		#im_file = 'test_0-3.bmp'
+		# Open image
+		#im_file = "C:/Users/borbs/OneDrive/04_Job Stuff/03_Work Stuff/PERLER_BEAD_MACHINE_00000/regions/test_0-0.bmp"
+		im = Image.open(os.path.join(regions_dir, im_file))
+		px = im.load()
 
-	region_num = im_file.rpartition(".")[0].rpartition("_")[2]
-	logger.info('Region num: {0}'.format(region_num))
+		region_num = im_file.rpartition(".")[0].rpartition("_")[2]
+		logger.info('Region num: {0}'.format(region_num))
 
-	logger.info('Image Format: {0}'.format(im.format))
-	logger.info('Image Size: {0}'.format(im.size))
-	logger.info('Image Mode: {0}'.format(im.mode))
+		logger.info('Image Format: {0}'.format(im.format))
+		logger.info('Image Size: {0}'.format(im.size))
+		logger.info('Image Mode: {0}'.format(im.mode))
 
-	x_size, y_size = im.size
-	logger.info('Width: {0}'.format(x_size))
-	logger.info('Height: {0}'.format(y_size))
+		x_size, y_size = im.size
+		logger.info('Width: {0}'.format(x_size))
+		logger.info('Height: {0}'.format(y_size))
 
-	json_data[region_num] = {}
+		json_data[region_num] = {}
 
-	for y in range(0, int(y_size)):
-		for x in range(0, int(x_size)):
-			logger.debug('x coord: {0}'.format(x))
-			logger.debug('y coord: {0}'.format(y))
-			logger.debug('pixel value: {0}'.format(px[x,y]))
+		for y in range(0, int(y_size)):
+			for x in range(0, int(x_size)):
+				logger.debug('x coord: {0}'.format(x))
+				logger.debug('y coord: {0}'.format(y))
+				logger.debug('pixel value: {0}'.format(px[x,y]))
 
-			json_data[region_num]['{0}-{1}'.format(y,x)] = px[x,y] 
+				json_data[region_num]['{0}-{1}'.format(y,x)] = px[x,y] 
 
-with open(json_file, "w") as outfile:
-	json.dump(json_data, outfile, sort_keys=True, 
-	indent=4, separators=(',', ': ')
-)
+	with open(json_file, "w") as outfile:
+		json.dump(json_data, outfile, sort_keys=True, 
+		indent=4, separators=(',', ': ')
+	)
 
-# Region dict example
+	# Region dict example
 
-'''
-regions : {
-	1-1: {
-		1-1: (255,255,255),
-		1-2: (255,255,255),
-		1-2: (255,255,255),
-		2-2: (255,255,255)
-	},
-	1-2: {
-		1-1: (255,0,0),
-		1-2: (255,0,0),
-		1-2: (255,0,0),
-		2-2: (255,0,0)
+	'''
+	regions : {
+		1-1: {
+			1-1: (255,255,255),
+			1-2: (255,255,255),
+			1-2: (255,255,255),
+			2-2: (255,255,255)
+		},
+		1-2: {
+			1-1: (255,0,0),
+			1-2: (255,0,0),
+			1-2: (255,0,0),
+			2-2: (255,0,0)
+		}
 	}
-}
-'''
+	'''
 
+except:
+	logger.warning('No regions found. Run image_split to create regions images')
